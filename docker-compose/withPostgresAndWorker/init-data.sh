30b2b43771f44c8151fb3b05125391da1adddedf
uihoh0 <uihoh0@gmail.com>
2024-07-11 22:19:10 +0800

    Update init-data.sh

--- docker-compose/withPostgresAndWorker/init-data.sh
+++ docker-compose/withPostgresAndWorker/init-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e;
-
+echo 'start initial'
 
 if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
@@ -11,3 +11,5 @@ if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}
 else
 	echo "SETUP INFO: No Environment variables given!"
 fi
+
+echo 'end initial'
